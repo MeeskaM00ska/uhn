1cb7c0f2a879a2926c80575856bb46d5ae7bea20
MeeskaM00ska <nathannnnn.li@mail.utoronto.ca>
2023-02-21 15:11:56 +0800

    deploy

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const pg = require("pg");
+const cors = require("cors");
+
+
+const config = {
+  host: 'localhost',
+  // Do not hard code your username and password.
+  // Consider using Node environment variables.
+  user: 'postgres',
+  password: '123456',
+  database: 'postgres',
+  port: 5432,
+  //ssl: true
+};
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json());
+
+
+const client = new pg.Client(config);
+
+
+
+async function initTable() {
+  const query = `
+    CREATE TABLE IF NOT EXISTS user_progress(
+       PrescriptionID text,
+       Name text,
+       DoctorName text,
+       ExerciseName text,
+       NumberOfCompletedSets INTEGER,
+       FinishStatus text
+    )`;
+  return client.query(query)
+    .then(() => {
+      console.log('Table created successfully!');
+     // client.end(console.log('Closed client connection'));
+    })
+    .catch(err => console.log(err))
+}
+
+app.get('', async (req, res) => {
+  try {
+    const query = `SELECT * FROM user_progress`;
+    const queryRes = await client.query(query);
+    res.status(200).json([...queryRes.rows]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+})
+
+app.post('/save', async (req, res) => {
+  try {
+    const {
+      PrescriptionID,
+      Name,
+      DoctorName,
+      ExerciseName,
+      NumberOfCompletedSets,
+      FinishStatus
+    } = req.body;
+
+    const query = `
+      INSERT INTO user_progress(
+        PrescriptionID,
+        Name,
+        DoctorName,
+        ExerciseName,
+        NumberOfCompletedSets,
+        FinishStatus
+      ) VALUES($1, $2, $3, $4, $5, $6)
+    `;
+    await client.query(
+      query,
+      [PrescriptionID, Name, DoctorName, ExerciseName, NumberOfCompletedSets, FinishStatus]
+    );
+    res.status(201).send();
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+})
+
+client.connect(err => {
+  if (err) throw err;
+  else {
+    initTable()
+      .then(() => {
+        app.listen(PORT, () => {
+          console.log("The server set up at port " + PORT);
+        })
+      })
+
+  }
+});
